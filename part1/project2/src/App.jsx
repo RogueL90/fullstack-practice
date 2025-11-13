9cdfd69a2a5d71206afc822a4183161c9010d3d5
RogueL90 <pranayj.493@gmail.com>
2025-11-12 18:48:34 -0800

    More useState practice and functions calling functions

---
+++ part1/project2/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+const Button = (props) => {
+return(
+    <button onClick = {props.onClick}>{props.text}</button>
+)
+}
+const Display = (props) => <p>{props.value}</p>
+const App = () => {
+  const [value, setValue] = useState(10)
+  const setToValue = (val) => {
+    console.log("new value: ", val);
+    setValue(val);
+  }
+  return(
+<div>
+  <Display value = {value}/>
+  <Button onClick = {() => setToValue(1000)} text = "thousand" />
+  <Button onClick = {() => setToValue(0)} text = "zero" />
+  <Button onClick = {() => setToValue(value+1)} text = "increment" /> 
+</div>
+  )
+}
+export default App;
