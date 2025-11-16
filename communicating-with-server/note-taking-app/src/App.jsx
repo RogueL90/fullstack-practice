cddb41122ad9b426f491159ba13b293f53875113
RogueL90 <pranayj.493@gmail.com>
2025-11-15 23:05:23 -0800

    added forms to note taking app

--- communicating-with-server/note-taking-app/src/App.jsx
+++ communicating-with-server/note-taking-app/src/App.jsx
@@ -1,13 +1,46 @@
 import Notes from './components/Notes'
-const App = ({ notes }) => {
+import {useState } from 'react'
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState('set new note');
+  const [showAll, setShowAll] = useState(false)
+  
+  
+  const addNote = (note) => {
+    note.preventDefault()
+    const noteObject = {
+    content: newNote,
+    important: Math.random() < 0.5,
+    id: notes.length + 1,
+  }
+  setNotes(notes.concat(noteObject));
+  setNewNote('')
+  }
+
+  const handleNoteChange = (event) =>{
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll ? notes : notes.filter(note => note.important);
+console.log(showAll);
+
   return(
     <div>
       <h1>Notes</h1>
+       <div>
+        <button onClick = {() => setShowAll(!showAll)} >show {showAll ? 'important' : 'all'}</button>
+      </div>
       <ul>
-        {notes.map(note => 
+        {notesToShow.map(note => 
           <Notes key = {note.id} note = {note} />
         )}
       </ul>
+      <form onSubmit = {addNote}>
+        <input value = {newNote} onChange = {handleNoteChange}/>
+        <button type = "Submit">save</button>
+      </form>
+      <br />
     </div>
   )
 }
